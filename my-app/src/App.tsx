19100cfe1f7ecf982787fd975584f3cdc4761b96
Rodney <machr@wit.edu>
2025-06-30 19:16:58 -0400

    Website Testing

---
+++ my-app/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank">
+        <h1>DO NOT CLICK</h1>
+      </a>
+    </>
+  );
+}
+
+export default App;
