1fd8aecb720ce38b9e427ea835cbd2e6ad141877
Rodney <machr@wit.edu>
2025-07-25 16:04:13 -0400

    Fixed with prettier

--- server/server.js
+++ server/server.js
@@ -1,32 +1,29 @@
-import dotenv from 'dotenv';
-import express from 'express';
-import cors from 'cors';
-import bodyParser from 'body-parser';
-import fetch from 'node-fetch';
+import dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import fetch from "node-fetch";
 dotenv.config();
 const app = express();
 const PORT = 3001;
-const WEBHOOK_URL = process.env.WEBHOOK_URL
-
-
+const WEBHOOK_URL = process.env.WEBHOOK_URL;
 
 app.use(cors());
 app.use(bodyParser.json());
 
-app.post('/api/contact', async (req, res) => {
+app.post("/api/contact", async (req, res) => {
   const { name, email, message } = req.body;
 
   if (!email || !message || !name) {
-    return res.status(400).json({ error: 'Missing fields' });
+    return res.status(400).json({ error: "Missing fields" });
   }
 
   try {
     await fetch(WEBHOOK_URL, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
-        content: 
-        `📩 **New Contact Form Submission**
+        content: `📩 **New Contact Form Submission**
 **Name**: ${name}
 **Email**: ${email}
 **Message**: ${message}`,
@@ -35,8 +32,8 @@ app.post('/api/contact', async (req, res) => {
 
     res.status(200).json({ success: true });
   } catch (err) {
-    console.error('Error sending to Discord:', err);
-    res.status(500).json({ error: 'Failed to send message' });
+    console.error("Error sending to Discord:", err);
+    res.status(500).json({ error: "Failed to send message" });
   }
 });
 
--- src/data/project.ts
+++ src/data/project.ts
@@ -10,18 +10,27 @@ export const Project: ProjectItem[] = [
     title: "Ezpub",
     stack: ["React", "Tailwind CSS", "TypeScript", "Vite", "Python", "JWT"],
     link: "https://github.com/angelomelo01/EZPub",
-    description: "An EPUB file reader that converts your ebooks into audiobooks."
+    description:
+      "An EPUB file reader that converts your ebooks into audiobooks.",
   },
   {
     title: "Skribble",
-    stack: ["React", "Tailwind CSS", "TypeScript", "Vite", "JavaScript", "Docker"],
+    stack: [
+      "React",
+      "Tailwind CSS",
+      "TypeScript",
+      "Vite",
+      "JavaScript",
+      "Docker",
+    ],
     link: "https://github.com/Alex-wit-2025/seniorproject",
-    description: "A note taking application with collaborative editing and LaTeX solutions."
+    description:
+      "A note taking application with collaborative editing and LaTeX solutions.",
   },
   {
     title: "Personal Website",
     stack: ["React", "Tailwind CSS", "TypeScript", "AWS"],
     link: "https://github.com/machratwit/personal-website",
-    description: "A personal website"
-  }
+    description: "A personal website",
+  },
 ];
